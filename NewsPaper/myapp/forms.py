80e02cc722733744fee0e8c73230c6d98db020d4
Ritter00 <olzim@tut.by>
2021-09-10 15:35:23 +0300

    forms

---
+++ NewsPaper/myapp/forms.py
@@ -0,0 +1,12 @@
+from django.forms import ModelForm
+from .models import Post
+from django import forms
+
+# Создаём модельную форму
+class PostForm(ModelForm):
+
+# в класс мета, как обычно, надо написать модель, по которой будет строится форма и нужные нам поля. Мы уже делали что-то похожее с фильтрами.
+    class Meta:
+        model = Post
+        fields = ['author','title', 'categoryType', 'postCategory', 'text']
+
--- NewsPaper/myapp/views.py
+++ NewsPaper/myapp/views.py
@@ -1,6 +1,8 @@
 from django.views.generic import ListView, DetailView
 from .models import Post
 from .filters import PostFilter
+from django.shortcuts import render
+from .forms import PostForm
 
 
 class PostList(ListView):
@@ -8,12 +10,22 @@ class PostList(ListView):
     template_name = 'posts.html'
     context_object_name = 'posts'
     queryset = Post.objects.order_by('-id') #  сортируем, еще можно через ordering = ['-id']
-    paginate_by = 3 # поставим постраничный вывод в n-элемент
+    paginate_by = 10 # поставим постраничный вывод в n-элемент
+    form_class = PostForm
 
     def get_context_data(self,**kwargs):
         context = super().get_context_data(**kwargs) # забираем отфильтрованные объекты переопределяя метод get_context_data у наследуемого класса
         context['filter']= PostFilter(self.request.GET, queryset=self.get_queryset())
+        context['form'] = PostForm()
         return context
+
+    def post(self, request,*args, **kwargs):
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+        return super().get(request, *args, **kwargs)
 
 class PostDetail(DetailView):
     model = Post
@@ -26,7 +38,8 @@ class PostListSearch(ListView):
     template_name = 'posts_search.html'
     context_object_name = 'posts'
     queryset = Post.objects.order_by('-id') #  сортируем, еще можно через ordering = ['-id']
-    paginate_by = 10 # поставим постраничный вывод в n-элемент
+    paginate_by = 5 # поставим постраничный вывод в n-элемент
+
 
     def get_filter(self):
         return PostFilter(self.request.GET, queryset= super().get_queryset())
